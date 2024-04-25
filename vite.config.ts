0bc5e0c74830b2b1aed626079edc25f2c9fec213
SahilMahadwar <accounts@mahadwar.com>
2024-04-26 01:21:19 +0530

    add vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
+import { vercelPreset } from "@vercel/remix/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [remix({ presets: [vercelPreset()] }), tsconfigPaths()],
+  server: {
+    open: true,
+    port: 3000,
+  },
 });
